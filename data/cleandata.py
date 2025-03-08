51cf7f107326d9eebfb347291c148dbc13f432d0
Rusnanee Madeng <rusnanee.m07@gmail.com>
2025-03-08 20:37:40 +0700

    add new data and cleaned data

---
+++ data/cleandata.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Load the CSV file into a pandas DataFrame
+file_path = 'D:\\7\\66-psu\\year2\\semester2\\ba ai\\termpro-predictPM25\\data\\pm-data.csv'
+data = pd.read_csv(file_path)
+
+
+# Drop columns with more than 50% NaN values
+data.dropna(axis=1, thresh=1, inplace=True)
+
+data['temperature'].fillna(data['temperature'].mean(), inplace=True)
+data['pm_2_5'].fillna(data['pm_2_5'].mean(), inplace=True)
+
+# Drop specific columns
+columns_to_drop = ["timezone", "pm_10", "pm_2_5_sp"]
+data.drop(columns=columns_to_drop, inplace=True, errors="ignore")
+
+# Handle zeros separately
+columns_to_handle_zeros = ['humidity', 'pm_2_5', 'temperature']
+
+
+new_data = data.drop(index=range(2000, len(data)))
+cleaned_file_path = 'D:\\7\\66-psu\\year2\\semester2\\ba ai\\termpro-predictPM25\\data\\cleaned_data.csv'
+new_data.to_csv(cleaned_file_path, index=False)
---
+++ data/data.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+# กำหนด path ของโฟลเดอร์ที่มีไฟล์ Excel
+folder_path = "d://7//66-psu//year2//semester2//ba ai//termpro-predictPM25//pm-data"
+output_file = "d://7//66-psu//year2//semester2//ba ai//termpro-predictPM25//data//pm-data.csv"
+
+# ค้นหาไฟล์ Excel ทั้งหมดในโฟลเดอร์
+excel_files = [f for f in os.listdir(folder_path) if f.endswith((".xlsx", ".xls"))]
+
+# อ่านและรวมข้อมูล
+df_list = []
+for file in excel_files:
+    file_path = os.path.join(folder_path, file)
+    df = pd.read_excel(file_path)  # อ่านไฟล์ Excel
+    df_list.append(df)
+
+# รวมข้อมูลทั้งหมด
+merged_df = pd.concat(df_list, ignore_index=True)
+
+# บันทึกเป็นไฟล์ CSV
+merged_df.to_csv(output_file, index=False, encoding="utf-8-sig")
+
+print(f"รวมไฟล์เสร็จสิ้น! ไฟล์ถูกบันทึกที่: {output_file}")
