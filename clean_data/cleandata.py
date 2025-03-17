71cc0c538418e6968e6893408d8c2cc481f7f5a1
Rusnanee Madeng <rusnanee.m07@gmail.com>
2025-03-17 17:23:25 +0700

    update

--- clean_data/cleandata.py
+++ clean_data/cleandata.py
@@ -1,25 +1,18 @@
 import pandas as pd
 
-# Load the CSV file into a pandas DataFrame
 file_path = 'D:\\7\\66-psu\\year2\\semester2\\ba ai\\termpro-predictPM25\\clean_data\\pm-data.csv'
 data = pd.read_csv(file_path)
-
-
 # Drop columns with more than 50% NaN values
 data.dropna(axis=1, thresh=1, inplace=True)
 
 data['temperature'].fillna(data['temperature'].mean(), inplace=True)
 data['pm_2_5'].fillna(data['pm_2_5'].mean(), inplace=True)
+data['humidity'].fillna(data['humidity'].mean(), inplace=True)
 
-# Drop specific columns
 columns_to_drop = ["timezone", "pm_10", "pm_2_5_sp", "Unnamed: 0"]
 data.drop(columns=columns_to_drop, inplace=True, errors="ignore")
 data['timestamp'] = data['timestamp'].str.split('.').str[0]
-
-
-# Handle zeros separately
 columns_to_handle_zeros = ['humidity', 'pm_2_5', 'temperature']
-
 
 new_data = data.tail(2000)
 cleaned_file_path = 'D:\\7\\66-psu\\year2\\semester2\\ba ai\\termpro-predictPM25\\clean_data\\cleaned_data.csv'
